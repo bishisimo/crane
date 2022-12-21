e6b38ea0a2aaf372596cc28e4e977fe8416470c6
aiden <bishisimo@qq.com>
2022-12-21 17:06:47 +0800

    feat(kubectx): add current flag for list
    <body>
    
    <footer>

--- app/kubectx/list.go
+++ app/kubectx/list.go
@@ -6,7 +6,11 @@ func (c *KubeCtx) List() error {
 	data := make([][]string, 0, len(c.metadata.Contexts)+1)
 	data = append(data, []string{"host", "name", "namespace", "cluster", "user"})
 	for _, v := range c.metadata.Contexts {
-		line := []string{v.Host, v.Name, v.Namespace, v.Cluster, v.User}
+		flag := "  "
+		if v.Host == c.metadata.Current {
+			flag = "* "
+		}
+		line := []string{flag + v.Host, v.Name, v.Namespace, v.Cluster, v.User}
 		data = append(data, line)
 	}
 	ui.Table(data)
